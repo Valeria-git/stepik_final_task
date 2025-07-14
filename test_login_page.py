063f12ee83f486ee6ffee5d5dce2c550a0b33b42
Valeriya Bahtinova <valeriya@Valeriyas-MacBook.local>
2025-07-14 23:11:17 +0200

    separate tests into two files

---
+++ test_login_page.py
@@ -0,0 +1,25 @@
+from test_data import LOGIN_PAGE_LINK
+from test_data import MAIN_PAGE_LINK
+from pages.login_page import LoginPage
+from pages.main_page import MainPage
+
+def test_guest_can_go_to_login_page(browser):
+    page = MainPage(browser, MAIN_PAGE_LINK)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+
+def test_guest_should_see_login_form(browser):
+    page = LoginPage(browser, LOGIN_PAGE_LINK)
+    page.open()
+    page.should_be_login_form()
+    
+
+def test_guest_should_see_registration_form(browser):
+    page = LoginPage(browser, LOGIN_PAGE_LINK)
+    page.open()
+    page.should_be_register_form()
+
+
--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,7 @@
 from pages.main_page import MainPage
-from pages.login_page import LoginPage
 import pytest
 from test_data import MAIN_PAGE_LINK
-from test_data import LOGIN_PAGE_LINK
+
 
 
        
@@ -10,25 +9,5 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, MAIN_PAGE_LINK)
     page.open()
     page.should_be_login_link()
-
-
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, MAIN_PAGE_LINK)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-
-
-def test_guest_should_see_login_form(browser):
-    page = LoginPage(browser, LOGIN_PAGE_LINK)
-    page.open()
-    page.should_be_login_form()
-    
-
-def test_guest_should_see_registration_form(browser):
-    page = LoginPage(browser, LOGIN_PAGE_LINK)
-    page.open()
-    page.should_be_register_form()
 
 
