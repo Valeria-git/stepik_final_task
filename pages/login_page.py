ee879ce29ac537744c7cc9d9d5d3afe2578f6df1
Valeriya Bahtinova <valeriya@Valeriyas-MacBook.local>
2025-07-14 22:15:46 +0200

    adding new 3 test and logic for them, modifing .gitignore

---
+++ pages/login_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        actual_url = self.browser.current_url
+        expected_url = LoginPageLocators.LOGIN_PAGE
+        assert actual_url == expected_url, f"Expected URL to be {expected_url}, but got {actual_url}"
+
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,6 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
+import pytest
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -13,3 +15,23 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_should_see_login_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+def test_guest_should_see_login_form(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_guest_should_see_registration_form(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
+
