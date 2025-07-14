b3be0c110b074cbbf78e7f72ef0130fa9201906c
Valeriya Bahtinova <valeriya@Valeriyas-MacBook.local>
2025-07-14 22:38:20 +0200

    addting test_data.py, adding implicit switch between pages

---
+++ test_data.py
@@ -0,0 +1,2 @@
+MAIN_PAGE_LINK = "http://selenium1py.pythonanywhere.com/"
+LOGIN_PAGE_LINK = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
--- test_main_page.py
+++ test_main_page.py
@@ -1,36 +1,33 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 import pytest
+from test_data import MAIN_PAGE_LINK
+from test_data import LOGIN_PAGE_LINK
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-
-
+       
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = MainPage(browser, MAIN_PAGE_LINK)
     page.open()
     page.should_be_login_link()
 
-def test_guest_should_see_login_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
+
+def test_guest_can_go_to_login_page(browser):
+    page = MainPage(browser, MAIN_PAGE_LINK)
     page.open()
-    page.should_be_login_url()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
 
 def test_guest_should_see_login_form(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
+    page = LoginPage(browser, LOGIN_PAGE_LINK)
     page.open()
     page.should_be_login_form()
+    
 
 def test_guest_should_see_registration_form(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
+    page = LoginPage(browser, LOGIN_PAGE_LINK)
     page.open()
     page.should_be_register_form()
 
