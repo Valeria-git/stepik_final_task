94d68e8e78aacfce1844979344a1d1008992904c
Valeriya Bahtinova <valeriya@Valeriyas-MacBook.local>
2025-07-15 16:27:31 +0200

    adding 3 new cases in test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -7,7 +7,7 @@ from pages.product_page import ProductPage
 import pytest
 import time
 
-#@pytest.mark.skip(reason="Just skip for testing another cases")
+@pytest.mark.skip(reason="Just skip for testing another cases")
 @pytest.mark.parametrize('link', [
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -32,17 +32,21 @@ def test_guest_can_add_product_to_basket(browser, link):
 
 
 @pytest.mark.xfail(reason="as message is presented for now")
-def test_success_message_disapper(browser):
-    page = ProductPage(browser, PRODUCT_PAGE_CODERS)
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, PRODUCT_PAGE_PROMO)
     page.open()
+    page.should_be_be_added_thing_in_basket_with_success()
     page.should_not_be_success_message()
-    page.should_be_be_added_thing_in_basket_with_success()    
-    page.success_message_should_disappear()
 
 
-def test_success_message_disapper1(browser):
+def test_guest_cant_see_success_message(browser):
     page = ProductPage(browser, PRODUCT_PAGE_PROMO)
     page.open()
     page.should_not_be_success_message()
-    page.should_be_be_added_thing_in_basket_with_success() 
-    time.sleep(100)  
+
+@pytest.mark.xfail(reason="as message is presented for now")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, PRODUCT_PAGE_PROMO)
+    page.open()
+    page.should_be_be_added_thing_in_basket_with_success()
+    page.success_message_should_disappear()
